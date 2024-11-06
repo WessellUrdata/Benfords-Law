55bce42b4dc231d301cd135cddab714e977dd64b
Wessell <git@wessell.dev>
2024-11-06 17:34:47 +0800

    Fixed local Python module import

---
+++ modules/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ImageForensics",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "opencv-python",
+        "scipy",
+        "scikit-learn",
+    ],
+    entry_points={
+        "console_scripts": [
+            # Define command-line scripts here if needed
+        ],
+    },
+)
